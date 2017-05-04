abb912379b2e4dca8180e79edcbe12dd0487ad06
Wishnu <Wishnu>
2017-05-04 20:40:43 +0530

    initial

---
+++ app/src/main/java/com/example/user/ecosnap/MainActivity.java
@@ -0,0 +1,180 @@
+package com.example.user.ecosnap;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    private Boolean isMapsFabOpen = false,isImageFabOpen=false;
+
+    private FloatingActionButton maps_fab,image_fab,feed_fab,recent_map,migration_map,geography_map,alien_map,camera,gallary;
+    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        maps_fab= (FloatingActionButton) findViewById(R.id.maps);
+        image_fab= (FloatingActionButton) findViewById(R.id.image);
+        feed_fab= (FloatingActionButton) findViewById(R.id.feed);
+        recent_map= (FloatingActionButton) findViewById(R.id.recent);
+        migration_map= (FloatingActionButton) findViewById(R.id.migration);
+        alien_map= (FloatingActionButton) findViewById(R.id.alien);
+        geography_map= (FloatingActionButton) findViewById(R.id.geography);
+        camera= (FloatingActionButton) findViewById(R.id.camera);
+        gallary= (FloatingActionButton) findViewById(R.id.gallary);
+
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+
+
+        maps_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(isMapsFabOpen){
+                    maps_fab.startAnimation(rotate_backward);
+                    recent_map.startAnimation(fab_close);
+                    geography_map.startAnimation(fab_close);
+                    alien_map.startAnimation(fab_close);
+                    migration_map.startAnimation(fab_close);
+
+                    recent_map.setClickable(false);
+                    geography_map.setClickable(false);
+                    alien_map.setClickable(false);
+                    geography_map.setClickable(false);
+                    isMapsFabOpen = false;
+
+
+                } else {
+
+                    maps_fab.startAnimation(rotate_forward);
+                    recent_map.startAnimation(fab_open);
+                    geography_map.startAnimation(fab_open);
+                    alien_map.startAnimation(fab_open);
+                    migration_map.startAnimation(fab_open);
+
+                    recent_map.setClickable(true);
+                    geography_map.setClickable(true);
+                    alien_map.setClickable(true);
+                    migration_map.setClickable(true);
+                    isMapsFabOpen = true;
+                }
+
+
+
+            }
+        });
+
+        image_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(isImageFabOpen){
+
+                    image_fab.startAnimation(rotate_backward);
+                    camera.startAnimation(fab_close);
+                    gallary.startAnimation(fab_close);
+                    camera.setClickable(false);
+                    gallary.setClickable(false);
+                    isImageFabOpen = false;
+
+
+                } else {
+
+                    image_fab.startAnimation(rotate_forward);
+                    camera.startAnimation(fab_open);
+                    gallary.startAnimation(fab_open);
+                    camera.setClickable(true);
+                    gallary.setClickable(true);
+                    isImageFabOpen = true;
+                }
+
+            }
+        });
+
+        feed_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(takePictureIntent, RESULT_LOAD_IMAGE);
+
+                }
+
+            }
+        });
+        gallary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+
+            }
+        });
+
+        recent_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent maps_intent=new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(maps_intent);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            //ImageView imageView = (ImageView) findViewById(R.id.imageView4);
+            //imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+
+        }
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/user/ecosnap/MapsActivity.java
@@ -0,0 +1,87 @@
+package com.example.user.ecosnap;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private LocationManager locationManager;
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, new android.location.LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+                    mMap.animateCamera(cameraUpdate);
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Marker "));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            }); //You can also use LocationManager.GPS_PROVIDER and LocationManager.PASSIVE_PROVIDER
+        }
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+}
